ee83e5acfbc42a91d2a512fcaf7207f507871aaa
Jay Moore <nq4t@nq4t.com>
2023-04-12 16:13:52 +0000

    Updated getv4

--- getv4.sh
+++ getv4.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 # GetV4 IP Check Service for pfSense - shell script
-# APR-10-2023 - Jay Moore (dewdude@pickmy.org)
+# APR-12-2023 - Jay Moore (dewdude@pickmy.org)
 # https://git.pickmy.org/dewdude/HEDyn
 
-# Modify "em0" to match your interface name. Place in
+# Modify `dev` to match your interface name. Place in
 # /usr/local/www along with getv4.php.
 
 # Make sure script is +x
 
-ifconfig em0 | grep inet | awk -F '[ \t]+|/' '{print $3}' | grep -v ^fe80
+dev=em0
+
+ifconfig $dev | awk '/inet/ && !/inet6/ {print $2}'
+#ip -4 addr show dev $dev | awk '/inet/ {print $2}' # for machines using `ip`
